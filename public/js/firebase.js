bdf7ce5a94ef7475d618bc4d7736dac7a311ccfb
unknown <nbenelfakir@gmail.com>
2023-12-01 10:40:44 +0100

    StellarBlog here your universal Blogging start

---
+++ public/js/firebase.js
@@ -0,0 +1,13 @@
+
+let firebaseConfig = {
+    apiKey: "AIzaSyDWW5d85sDCfOFNv4OguooIitJbUfGZr3s",
+    authDomain: "stellarblog-c418e.firebaseapp.com",
+    projectId: "stellarblog-c418e",
+    storageBucket: "stellarblog-c418e.appspot.com",
+    messagingSenderId: "393155055230",
+    appId: "1:393155055230:web:12781f7bd537afe24b122f"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
